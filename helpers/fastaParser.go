ae3f9b44ed2e71c9e417f5346db89e89f597b2f5
laulonskov98 <lauritslonskov@gmail.com>
2022-09-16 13:32:02 +0200

    f

--- helpers/fastaParser.go
+++ helpers/fastaParser.go
@@ -62,3 +62,37 @@ func GeneralParser(file string, format Format) []Recs {
 
 	return recs
 }
+
+func GeneralParserStub(file string, format Format) []Recs {
+	output := ""
+	fileScanner := bufio.NewScanner(strings.NewReader(file))
+	activeRec := new(Recs)
+
+	recs := make([]Recs, 0)
+	//scan file line by line
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		if len(line) == 0 {
+			continue
+		}
+
+		//handle 'name of sequence' cases
+		if line[0:1] == string(format) {
+			if len(output) != 0 {
+				activeRec.Rec = output
+				recs = append(recs, *activeRec)
+			}
+			output = ""
+			activeRec = new(Recs)
+			activeRec.Name = strings.TrimSpace(line[1:])
+			//handle 'sequence' cases
+		} else {
+			output = output + line
+		}
+	}
+	activeRec.Rec = output
+	recs = append(recs, *activeRec)
+
+	return recs
+}
--- helpers/sam.go
+++ helpers/sam.go
@@ -10,3 +10,9 @@ func Sam(readName string, chrom string, pos int, readString string) {
 
 	fmt.Print(output)
 }
+
+func SamStub(readName string, chrom string, pos int, readString string) string {
+	output := readName + "	" + chrom + "	" + fmt.Sprint(pos+1) + "	" + strconv.Itoa(len(readString)) + "M" + "	" + readString + "\n"
+
+	return output
+}
